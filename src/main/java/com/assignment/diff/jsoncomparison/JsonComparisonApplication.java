17550d49bbf2b910c91648778017067c40464298
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2019-07-19 11:55:49 +0300

    Initial commit

---
+++ src/main/java/com/assignment/diff/jsoncomparison/JsonComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.diff.jsoncomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonComparisonApplication.class, args);
+	}
+
+}
